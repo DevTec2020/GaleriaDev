e45463434ca7a924db0419f548e33eda6f79c20a
DevTec2020 <leonardo.silva.inf@gmail.com>
2025-01-13 20:31:22 -0300

    FEAT: Navbar Criada

--- src/Navbar/index.tsx
+++ src/Navbar/index.tsx
@@ -1,18 +1,23 @@
-import searchIco from "../assets/search.svg"
+import SearchIco from "../assets/search.svg"
+import PersonIco from "../assets/person.png"
 import GaleriaDevIco from "../../public/GaleriaIco.png"
 
 export function Navbar() {
     return (
-        <nav className="flex justify-around">
+        <nav className="flex justify-around pt-10 pb-4 bg-slate-100">
                 <div className="flex ">
                     <img className="h-12" src= {GaleriaDevIco} alt="Icone Galeria" />
-                    <span className="flex justify-center items-center">Galeria Dev</span>
+                    <span 
+                        className="flex justify-center items-center text-orange-600 text-lg font-bold">
+                        Galeria Dev
+                    </span>
                 </div>
 
                 <div className="flex gap-5 justify-center items-center">
-                    <img className="h-5 flex items-baseline" src={searchIco} alt="Icone de Busca" />
-                    <span>Explorer</span>
-                    <span>Favoritos</span>
+                    <img className="h-5" src={SearchIco} alt="Icone de Busca" />
+                    <span className="font-medium text-slate-500 hover:text-orange-600 hover:border-orange-600 hover:border-b-2">Explorer</span>
+                    <span className="font-medium text-slate-500 hover:text-orange-600 hover:border-orange-600 hover:border-b-2">Favoritos</span>
+                    <img className="h-9 " src={PersonIco} alt="Icone de usuário" />
                 </div>
 
         </nav>
