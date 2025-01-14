b9cb622fbeb0d0bdb8e62160f00a45dba118e3e6
DevTec2020 <leonardo.silva.inf@gmail.com>
2025-01-13 22:27:05 -0300

    FEAT: Montando main com as imagens coletadas da API

--- src/Galeria/index.tsx
+++ src/Galeria/index.tsx
@@ -1,8 +1,37 @@
+import { useState, useEffect } from "react"
+
+interface Imagem {
+    id: string
+    author: string
+    download_url:string
+}
 
 export function Galeria(){
+    const [Imagens, setImagens] = useState<Imagem[]>([]);
+
+    // Importando imagem da API
+    useEffect(() => {
+        fetch('https://picsum.photos/v2/list')
+            .then((response) => response.json())
+            .then((data) => setImagens(data))
+            .catch((error) => console.error("Erro ao carregar imagens", error));
+    },[]);
+
+
     return (
-        <div>
-            <img src="" alt="" />
-        </div>
+        <div className="flex items-center justify-center mt-10">
+            <div className="container flex justify-center flex-wrap gap-2 ">
+                {Imagens.map((img) => (
+                    <div key={img.id}>
+                        <img 
+                            src={`${img.download_url}?w=200&h=150`} 
+                            alt={`Foto de ${img.author}`}
+                            className="h-48 w-56"
+                            loading="lazy"
+                        />
+                    </div>
+                ))}
+            </div>
+        </div>    
     )
 }
