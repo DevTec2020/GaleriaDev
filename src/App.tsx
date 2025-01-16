0693d70d1ecd06a40337195063f521e7d758367f
DevTec2020 <leonardo.silva.inf@gmail.com>
2025-01-16 19:41:26 -0300

    feat: Preparando guia de favoritos

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Header } from "./components/Header" 
 import { Galeria } from "./components/Galeria"
+import { Favorites } from "./components/Favorites"
 import { Footer } from "./components/Footer"
 
 export function App() {
---
+++ src/components/Favorites/index.tsx
@@ -0,0 +1,10 @@
+export function Favorites() {
+        return (
+            <div className="flex items-center justify-center mt-10">
+                <div className="container flex justify-center flex-wrap gap-4 ">
+
+                    <p className="flex justify-center my-10 text-3xl font-bold text-orange-500">Favoritos...</p>                
+                </div>
+            </div>    
+        )        
+}
