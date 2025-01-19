3302d205ad1d48afcc85cfb1bdac9c131ee562c9
DevTec2020 <leonardo.silva.inf@gmail.com>
2025-01-18 23:02:54 -0300

    feat: Diminuindo texto do footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,7 +1,7 @@
 export function Footer() {
     return (
         <footer className="flex items-center justify-center py-5 mt-5 bg-slate-100">
-            <p className="text-sm">&copy; 2025 Galeria Dev. Feito por Leonardo Batista. Todos os direitos reservados.</p>
+            <p className="text-sm">&copy; 2025 Galeria Dev. Feito por Leonardo Batista.</p>
         </footer>
     )
 }
